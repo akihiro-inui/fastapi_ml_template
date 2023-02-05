a78f643ee61639a37ee9b34c84a2da3216aa7094
Akihiro Inui <mail@akihiroinui.com>
2023-02-05 13:52:29 +0100

    Added more models

--- ml/src/models/model_selector.py
+++ ml/src/models/model_selector.py
@@ -1,4 +1,9 @@
 from ml.src.models.cnn import SRCNN
+from ml.src.models.vdsr import VDSR
+from ml.src.models.espcn import ESPCN
+from ml.src.models.srresnet import SRResNet
+from ml.src.models.edsr import EDSR
+
 from common_tools.src.custom_error_handlers import ModelError
 
 
@@ -10,10 +15,18 @@ class ModelSelector:
         :param scale_factor: Scale factor for the model
         """
         self.srcnn = SRCNN(scale_factor)
+        self.vdsr = VDSR(scale_factor)
+        self.espcn = ESPCN(scale_factor)
+        self.srresnet = SRResNet(scale_factor)
+        self.edsr = EDSR(scale_factor)
 
         # Model selector
         self.model_map = {
-            "SRCNN": self.srcnn
+            "SRCNN": self.srcnn,
+            "VDSR": self.vdsr,
+            "ESPCN": self.espcn,
+            "SRResNet": self.srresnet,
+            "EDSR": self.edsr,
         }
 
     def select_model(self, model_name: str):
